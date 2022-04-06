6c7e5fa37794dbd0aa7d14ad86a7a9652455dcd1
Ocelot <100012348+billycwong19@users.noreply.github.com>
2022-04-06 01:33:21 -0500

    another commit with readjustments and notes for the homework

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,6 @@ var startContainer = document.querySelector(".startContainer")
 var startHeader = document.querySelector(".startHeader")
 var prompts = document.querySelector(".prompts")
 var startBtn = document.querySelector(".startBtn")
-
 var questionsContainer = document.querySelector(".container")
 var question = document.querySelector(".question")
 var answersList = document.querySelector(".answersList")
@@ -10,28 +9,24 @@ var answerA = document.querySelector(".answerA")
 var answerB = document.querySelector(".answerB")
 var answerC = document.querySelector(".answerC")
 var answerD = document.querySelector(".answerD")
-
 var result = document.querySelector(".result")
-
 var score = document.querySelector(".score")
 var correct = document.querySelector(".correct");
 var incorrect = document.querySelector(".incorrect")
-
 var restart = document.querySelector(".restart")
-
 var time = document.querySelector(".time")
-
 var form = document.querySelector("form")
 var initials = document.querySelector("#initials")
 var submitBtn = document.querySelector("#submitBtn");
 var highScorePage = document.querySelector(".highScorePage")
 var highScores = document.querySelector(".highScores")
-
+// these become my score counters
 var right = 0;
 var wrong = 0;
+// these become my iterators for questions and answers
 var x = 0;
-var i = 0;
-
+var i = 0; 
+// these are the questions and answers stored in an object
 var questionsAndAnswers = [
     {
     "question": "Finish this statement: I love _____",
@@ -45,6 +40,7 @@ var questionsAndAnswers = [
     "b": "I dont",
     "c": "This one's wrong",
     "d": "Click this one!"},
+    // "correct": "d"},
     {
     "question": "Who is your favorite singer?",
     "a": "Madonna",
@@ -89,6 +85,7 @@ var questionsAndAnswers = [
      "d": "All of the above"}
 ];
 
+// beginning of questioning. displays elements while hiding startContainer
 function startGame(){
     startContainer.setAttribute("style","display: none;")
     questionsContainer.setAttribute("style","display: initial;");
@@ -96,28 +93,31 @@ function startGame(){
     score.setAttribute("style","display: initial;");
     displayQuestion();
 }
-
+// displays one question at a time using the global x or it skips to displayScore
 function displayQuestion(){
-    question.textContent = questionsAndAnswers[x].question;
     if (x < questionsAndAnswers.length) {
+        question.textContent = questionsAndAnswers[x].question;
         x += 1
-    } else if ( i === questionsAndAnswers.length){
-        return displayScore();
+    } else {
+        displayScore();
     }
     displayAnswers();
 }
-
-function displayAnswers(){
+// displays answers from their corresponding location in the object array and awats a click event to being comparing
+function displayAnswers(){  
+    if (i < questionsAndAnswers.length) {
         answerA.textContent = questionsAndAnswers[i].a;
         answerB.textContent = questionsAndAnswers[i].b;
         answerC.textContent = questionsAndAnswers[i].c;
         answerD.textContent = questionsAndAnswers[i].d;
-        if (i < questionsAndAnswers.length) {
-            i += 1;
+        i += 1;
         } 
 }
-
+// compares if click event matches the value of a dataset attribute of a button. displays a string if you got it right or not
 function comparing(event){ 
+    // let x = 0;
+    // let correct = questionsAndAnswers[i].correct;
+    // console.log(correct)
     var compare = event.target;
     if (compare.matches("button")){
         if (compare.dataset.right === "right"){
@@ -133,21 +133,21 @@ function comparing(event){
         displayQuestion();
     }
 }
-// Took this function from the class activities
-var secondsLeft = 30;
+// Took this function from the class activities and counts down from 30 seconds. once it hits zero or less it stops. 
+var secondsLeft = 31;
 function setTime() {
     var timerInterval = setInterval(function() {
         secondsLeft--;
         time.textContent = secondsLeft;
-    if(secondsLeft < 0) {
+    if(secondsLeft <= 0) {
         clearInterval(timerInterval);
         displayScore();
         secondsLeft = 0
-        time.textContent = ""
+        time.textContent = 0
       }
     }, 1000);
 }
-
+// hides questionsContainer and stores score in the local storage. then retrieves the newScore to compare to the Old Score which is the previous high score. 
 function displayScore() {
     startContainer.setAttribute("style","display: none;")
     questionsContainer.setAttribute("style","display: none;")
@@ -157,7 +157,6 @@ function displayScore() {
 
     var oldScore = JSON.parse(localStorage.getItem("oldScore"));
     var newScore = JSON.parse(localStorage.getItem("newScore"));
-    console.log(newScore)
     if (newScore > oldScore) {
         localStorage.setItem("oldScore", JSON.stringify(newScore));
         newHighScore();
@@ -166,27 +165,29 @@ function displayScore() {
         restart.textContent = "Play again?"
     }
 }
-
+// displays a form if the newScore is greater than the oldScore
 function newHighScore(){
     form.setAttribute("style","display: initial;");
 }
-  
-
+// adds an li item with initial and score to the highScores list
+function createScoreList(event){
+    event.preventDefault;
+    var listItem = document.createElement('LI');
+    listItem.textContent = "Initials: " + initials.value + " | Score: " + JSON.parse(localStorage.getItem("newScore"));
+    highScores.insertBefore(listItem, highScores.firstChild)
+    console.log(listItem.textContent)
+    displayHighScores();
+}
+// displays high scores!
 function displayHighScores(){
     highScores.setAttribute("style","display: initial;")
     startContainer.setAttribute("style","display: none;")
     questionsContainer.setAttribute("style","display: none;")
     score.setAttribute("style","display: none;")
     form.setAttribute("style","display: none;");
-
-    let listItem = document.createElement('LI');
-    listItem.appendChild(document.createTextNode("NEW! Initials: " + initials.value + " | Score: " + JSON.parse(localStorage.getItem("newScore"))));
-    highScores.appendChild(listItem);
-    console.log(initials.value);
-
-    
+    result.setAttribute("style","display: none;");
 }
-
+// initializes game and acts as a reset as well.
 function init(){
     right = 0;
     wrong = 0;
@@ -206,13 +207,10 @@ function init(){
 }
 
 init();
-
+// event listeners!
 startBtn.addEventListener("click", startGame)
 startBtn.addEventListener("click", setTime)
 answersList.addEventListener("click", comparing)
 highScorePage.addEventListener("click", displayHighScores)
 restart.addEventListener("click", init)
-submitBtn.addEventListener("click", function(event){
-    event.preventDefault;
-    displayHighScores();
-});
+submitBtn.addEventListener("click", createScoreList)
